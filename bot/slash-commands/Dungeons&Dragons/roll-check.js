4eadc08656f4524a2ae1082ebf07acc6fa26235b
LennertJ <jans.lennert@gmail.com>
2021-10-18 10:14:15 +0200

    updated style to match !roll

--- bot/slash-commands/Dungeons&Dragons/roll-check.js
+++ bot/slash-commands/Dungeons&Dragons/roll-check.js
@@ -4,33 +4,42 @@ module.exports.run = async (interaction) => {
     let additionalModifier = interaction.options.getNumber('additional-modifier')==null? 0 : interaction.options.getNumber('additional-modifier');
 
     singleStat = [];
-    singleStat.push(Math.ceil( Math.random()*20)+additionalModifier);
+    singleStat.push(Math.ceil( Math.random()*20));
     if(rolltwice){
-        singleStat.push(Math.ceil( Math.random()*20)+additionalModifier);
+        singleStat.push(Math.ceil( Math.random()*20));
     }
     singleStat = singleStat.sort((a,b) => b-a);
     let command = "1d20 + " + additionalModifier;
 
-    return interaction.reply({ embeds: [createEmbed(command,singleStat)] })
+    return interaction.reply({ embeds: [createEmbed(command,singleStat,additionalModifier)] })
 }
 
-function createEmbed(command,singleStat){
+function createEmbed(command,singleStat,modifier) {
     if(singleStat.length==1){
         return new MessageEmbed()
         .setTitle(command)
-        .addField('\u200B', "**```"+singleStat[0]+"```**", true)
+        .addField('\u200B', `${showRightColourOfRolledDie(singleStat[0],modifier)}`, true)
         .setTimestamp()
     }
     else{
     return new MessageEmbed()
         .setTitle(command)
-        .addField('\u200B', "**```"+singleStat[0]+"```**", true)
+        .addField('\u200B', `${showRightColourOfRolledDie(singleStat[0],modifier)}`, true)
         .addField('\u200B', '\u200B', true)
-        .addField('\u200B', "**```"+singleStat[1]+"```**", true)
+        .addField('\u200B', `${showRightColourOfRolledDie(singleStat[1],modifier)}`, true)
         .setTimestamp()
     }
 }
 
+function showRightColourOfRolledDie(roll,modifier) {
+    if (roll == 1) {
+        return `\`\`\`fix\n ${(roll+modifier).toString()}\`\`\``;
+    } else if (roll == 20) {
+        return `\`\`\`xl\n ${(roll+modifier).toString()}\`\`\``;
+    } else {
+        return `\`\`\`md\n ${(roll+modifier).toString()}\`\`\``;
+    }
+}
 module.exports.help = {
     name: 'roll-check',
     permission: [],
