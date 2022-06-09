5cf0d5e5008bcd9d53233487186370459937085b
S-Eberwijn <11700946@student.pxl.be>
2022-06-09 21:03:58 +0200

    adjustments to CI

--- index.js
+++ index.js
@@ -75,6 +75,20 @@ app.use('/', require('./website/routes/home'));
 app.use('/dashboard/', require('./website/routes/dashboard'));
 app.use('/auth', require('./website/routes/auth'));
 
+// app.post("/refresh", async (req, res) => {
+//     console.log("repl.deploy" + req.body + req.headers.get("Signature"))
+
+//     const result = JSON.parse((await getStdinLine()))
+
+//     await res.setStatus(result.status).end(result.body)
+//     console.log("repl.deploy-success")
+// })
+// async function getStdinLine() {
+//     for await (const line of readLines(Deno.stdin)) {
+//         return line
+//     }
+// }
+
 
 // Initialize Discord Bot
 const { Client, Intents } = require('discord.js');
