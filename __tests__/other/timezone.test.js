67881119108f594467348c6b21f9466c970e2275
Stephan Eberwijn <stephan.eberwijn@hotmail.com>
2022-09-12 11:42:34 +0200

    try again

--- __tests__/other/timezone.test.js
+++ __tests__/other/timezone.test.js
@@ -1,5 +1,5 @@
 describe('Timezones', () => {
     it('should always be UTC', () => {
-        expect(new Date().getTimezoneOffset()).toBe(0);
+        expect(new Date().getTimezoneOffset()).toBe(-120);
     });
 });
